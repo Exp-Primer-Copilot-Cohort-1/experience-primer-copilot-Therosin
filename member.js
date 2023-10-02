84bcd82593f54bad2cb40ee030e3dbb7bb4475f2
Theros <theros@svaltek.xyz>
2023-10-02 22:58:11 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,65 @@
+function skillsMember() {
+    var member = this;
+    member.skills = [
+        {
+            title: 'HTML',
+            level: '90'
+        },
+        {
+            title: 'CSS',
+            level: '90'
+        },
+        {
+            title: 'JavaScript',
+            level: '80'
+        },
+        {
+            title: 'jQuery',
+            level: '80'
+        },
+        {
+            title: 'PHP',
+            level: '70'
+        },
+        {
+            title: 'MySQL',
+            level: '70'
+        },
+        {
+            title: 'WordPress',
+            level: '70'
+        },
+        {
+            title: 'Bootstrap',
+            level: '80'
+        },
+        {
+            title: 'Sass',
+            level: '60'
+        },
+        {
+            title: 'Git',
+            level: '60'
+        },
+        {
+            title: 'Gulp',
+            level: '60'
+        },
+        {
+            title: 'Grunt',
+            level: '60'
+        },
+        {
+            title: 'AngularJS',
+            level: '50'
+        },
+        {
+            title: 'ReactJS',
+            level: '50'
+        },
+        {
+            title: 'NodeJS',
+            level: '50'
+        }
+    ];
+}
